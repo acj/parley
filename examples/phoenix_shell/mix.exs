ce8779a934bffe37add345a7a96eab1ac9925fe0
Adam Jensen <acjensen@gmail.com>
2017-04-09 18:45:59 -0400

    Use local path for parley dependency in the phoenix example

--- examples/phoenix_shell/mix.exs
+++ examples/phoenix_shell/mix.exs
@@ -38,7 +38,7 @@ defmodule PhoenixShell.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:parley, github: "acj/parley"}
+     {:parley, path: "../../"}
     ]
   end
 
