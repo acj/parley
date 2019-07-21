56b5c7b34f39c74af6ccd4a85a23ceccfc117bdf
Adam Jensen <acjensen@gmail.com>
2019-07-21 19:16:10 -0400

    Update to elixir 1.8

--- examples/phoenix_shell/mix.exs
+++ examples/phoenix_shell/mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixShell.Mixfile do
   def project do
     [app: :phoenix_shell,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.8",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
